14ad1155d1a346c96b3942fb460fd0e4f485142e
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-09-21 00:46:37 +0200

    edits (non-interactive mode)

--- shell.c
+++ shell.c
@@ -2,72 +2,91 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 #define MAX_INPUT_LENGTH 1024
 
 /**
-* main - Entry point for the simple shell program.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
+ * main - Entry point of the shell program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int main(int argc, char *argv[]) {
     char input[MAX_INPUT_LENGTH];
+    int is_interactive = isatty(STDIN_FILENO);  /* Check if standard input is a terminal */
 
-    while (1)
-    {
-        /* Display a prompt */
-        printf("#cisfun$ ");
-        fflush(stdout);
+    if (argc == 1 || (argc == 2 && is_interactive)) {
+        /* Interactive mode: no arguments provided or single argument in interactive mode */
+        while (1) {
+            if (is_interactive) {
+                printf("($) ");
+                fflush(stdout);
+            }
 
-        /* Read user input */
-        if (fgets(input, sizeof(input), stdin) == NULL)
-        {
-            /* Handle end of file (Ctrl+D) */
-            printf("\n");
-            break;
+            if (fgets(input, sizeof(input), stdin) == NULL) {
+                if (is_interactive) {
+                    printf("\n");
+                }
+                break;
+            }
+
+            input[strcspn(input, "\n")] = '\0';
+
+            if (access(input, X_OK) == 0) {
+                pid_t child_pid = fork();
+
+                if (child_pid == -1) {
+                    perror("Fork error");
+                    return (1);
+                }
+
+                if (child_pid == 0) {
+                    execlp(input, input, NULL);
+                    perror("Command execution error");
+                    exit(EXIT_FAILURE);
+                } else {
+                    int status;
+                    wait(&status);
+
+                    if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                        printf("Error: Command exited with status %d\n", WEXITSTATUS(status));
+                    }
+                }
+            } else {
+                printf("./shell: No such file or directory\n");
+            }
         }
-
-        /* Remove newline character from input */
-        input[strcspn(input, "\n")] = '\0';
-
-        /* Try to execute the command */
-        if (access(input, X_OK) == 0)
-        {
-            /* The command is executable */
+    } else if (argc == 2) {
+        /* Non-interactive mode: one argument provided */
+        if (access(argv[1], X_OK) == 0) {
             pid_t child_pid = fork();
 
-            if (child_pid == -1)
-            {
+            if (child_pid == -1) {
                 perror("Fork error");
                 return (1);
             }
 
-            if (child_pid == 0)
-            {
-                /* Child process */
-                execlp(input, input, NULL);
-
-                /* If execlp returns, an error occurred */
+            if (child_pid == 0) {
+                execlp(argv[1], argv[1], NULL);
                 perror("Command execution error");
                 exit(EXIT_FAILURE);
-            }
-            else
-            {
-                /* Parent process */
+            } else {
                 int status;
                 wait(&status);
 
-                if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+                if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
                     printf("Error: Command exited with status %d\n", WEXITSTATUS(status));
+                }
             }
-        }
-        else
-        {
-            /* The command is not executable */
+        } else {
             printf("./shell: No such file or directory\n");
         }
+    } else {
+        printf("Usage: %s [command]\n", argv[0]);
+        return (1);
     }
 
     return (0);
