7279e09d3faddb409e4a4f8af16de868132ccdc2
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-10-16 01:22:21 +0200

    Betty (exe_command.c:44: WARNING: line over 80)

--- A2/execute_command.c
+++ A2/execute_command.c
@@ -4,30 +4,45 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int execute_command(char *command) {
-    pid_t pid = fork();
+/**
+* execute_command - Attempts to execute a command from input.
+* @command: A pointer to the command.
+*
+* Return: 1
+*
+* Description: Parses the input as a command, else error.
+*              Creates a child process, else error.
+*/
+int execute_command(char *command)
+{
+	pid_t pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 
-    if (pid == 0) {
-        /* In the child process, execute the command */
-        if (execlp(command, command, NULL) == -1) {
-            /* If execlp returns, the command execution failed */
-            perror(command);
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        /* In the parent process, wait for the child to complete */
-        int status;
-        waitpid(pid, &status, 0);
+	if (pid == 0)
+	{
+		/* In the child process, execute the command */
+		if (execlp(command, command, NULL) == -1)
+		{
+			/* If execlp returns, the command execution failed */
+			perror(command);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* In the parent process, wait for the child to complete */
+		int status;
 
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-            fprintf(stderr, "./shell: %s: Exit status %d\n", command, WEXITSTATUS(status));
-        }
-    }
+		waitpid(pid, &status, 0);
 
-    return (1);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			fprintf(stderr, "./shell: %s: Exit status %d\n", command, WEXITSTATUS(status));
+	}
+
+	return (1);
 }
--- A2/main.c
+++ A2/main.c
@@ -4,25 +4,36 @@
 
 #define MAX_INPUT_LENGTH 1024
 
-int main(void) {
-    char input[MAX_INPUT_LENGTH];
+/**
+* main - Entry point to the shell program.
+*
+* Return: 0
+*
+* Description: Contains the shell program's main `while` loop.
+*              Displays the prompt, handles EOF and commands.
+*/
+int main(void)
+{
+	char input[MAX_INPUT_LENGTH];
 
-    while (1) {
-        printf("#cisfun$ ");  /* Display the prompt */
-        fflush(stdout);
+	while (1)
+	{
+		printf("#cisfun$ ");  /* Display the prompt */
+		fflush(stdout);
 
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            /* Handle Ctrl+D (end of file) */
-            printf("\n");
-            break;
-        }
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			/* Handle Ctrl+D (end of file) */
+			printf("\n");
+			break;
+		}
 
-        /* Remove the trailing newline character */
-        input[strcspn(input, "\n")] = '\0';
+		/* Remove the trailing newline character */
+		input[strcspn(input, "\n")] = '\0';
 
-        /* Execute the command */
-        execute_command(input);
-    }
+		/* Execute the command */
+		execute_command(input);
+	}
 
-    return (0);
+	return (0);
 }
