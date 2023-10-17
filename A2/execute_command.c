cef122069eb958cb008a7a0bd12789c065ae8965
Kenan Kotzee <frostcolliyone@gmail.com>
2023-10-17 14:44:29 -0700

    shell man test

--- A2/execute_command.c
+++ A2/execute_command.c
@@ -3,16 +3,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include "simple_shell.h"
 /**
-* execute_command - Attempts to execute a command from input.
-* @command: A pointer to the command.
-*
-* Return: 1
-*
-* Description: Parses the input as a command, else error.
-*              Creates a child process, else error.
-*/
+ * execute_command - Execute a shell command in a child process.
+ *
+ * @command: A string with the command and arguments.
+ *
+ * This function forks a new process to execute the provided command and
+ * checks for errors during the process creation and execution.
+ *
+ * Return: Always 1 (not indicative of command success).
+ */
+
 int execute_command(char *command)
 {
 	pid_t pid = fork();
@@ -25,24 +27,30 @@ int execute_command(char *command)
 
 	if (pid == 0)
 	{
-		/* In the child process, execute the command */
-		if (execlp(command, command, NULL) == -1)
+		char *args[32];
+		int arg_count = 0;
+
+		tokenize_command(command, args, &arg_count);
+
+		if (execvp(args[0], args) == -1)
 		{
-			/* If execlp returns, the command execution failed */
-			perror(command);
+			fprintf(stderr, "./shell: %s: not found\n", args[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
-		/* In the parent process, wait for the child to complete */
 		int status;
 
 		waitpid(pid, &status, 0);
 
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-			fprintf(stderr, "./shell: %s: Exit status %d\n", command, WEXITSTATUS(status));
+		{
+			fprintf(stderr, "./shell: %s: Exit status %d\n",
+					command, WEXITSTATUS(status));
+		}
 	}
 
 	return (1);
 }
+
--- A2/main.c
+++ A2/main.c
@@ -1,39 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "simple_shell.h"
 
 #define MAX_INPUT_LENGTH 1024
 
 /**
-* main - Entry point to the shell program.
-*
-* Return: 0
-*
-* Description: Contains the shell program's main `while` loop.
-*              Displays the prompt, handles EOF and commands.
-*/
+ * main - Entry point of the simple shell program
+ *
+ * Description:
+ * - This function sets up the main loop for the simple shell.
+ * - It repeatedly prompts the user for input, processes commands,
+ *   and either executes them or performs built-in actions.
+ *
+ * Return:
+ * - 0 on successful program termination.
+ */
+
 int main(void)
 {
 	char input[MAX_INPUT_LENGTH];
 
 	while (1)
 	{
-		printf("#cisfun$ ");  /* Display the prompt */
+		printf("#cisfun$ ");
 		fflush(stdout);
 
 		if (fgets(input, sizeof(input), stdin) == NULL)
 		{
-			/* Handle Ctrl+D (end of file) */
 			printf("\n");
 			break;
 		}
 
-		/* Remove the trailing newline character */
 		input[strcspn(input, "\n")] = '\0';
 
-		/* Execute the command */
-		execute_command(input);
+		if (strcmp(input, "exit") == 0)
+		{
+			break;
+		}
+		else if (strcmp(input, "env") == 0)
+		{
+			print_environment();
+		}
+		else
+		{
+			execute_command(input);
+		}
 	}
 
 	return (0);
 }
+
