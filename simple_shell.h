38b638f873591529e65fdbaa76abd86604935352
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-10-19 00:37:08 +0200

    Cleaned home dir

--- simple_shell.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
-
-void print_environment(void);
-void execute_command(const char *command);
-void tokenize_command(char *command, char *args[], int *arg_count);
-char *read_command();
-
-#endif
