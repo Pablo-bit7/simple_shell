813db11f91ae3b10b93f62416def2b37f74d9751
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2023-12-14 03:58:30 +0200

    House keeping

--- A2/simple_shell.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
-
-void print_environment(void);
-int execute_command(char *command);
-void tokenize_command(char *command, char *args[], int *arg_count);
-
-#endif
